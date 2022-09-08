02c7a3ca34e521720938fdf2753fffcf9e753207
betulince <btl.ince98@gmail.com>
2022-09-08 19:50:23 +0200

    HackerRank Java EOF Solution Added

---
+++ hackerRankJavaEOF/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int counter = 1;
+        while(sc.hasNext()) {
+            String inputText = sc.nextLine();
+            System.out.println(counter + " " + inputText);
+            counter++;
+        }
+    }
+}
