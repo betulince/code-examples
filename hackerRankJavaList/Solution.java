16a7994d0cbc7865331fbc66bf09610445ba6b4b
betulince <btl.ince98@gmail.com>
2022-09-17 16:13:39 +0200

    HackerRank Java List Solution Added

---
+++ hackerRankJavaList/Solution.java
@@ -0,0 +1,38 @@
+package hackerRankJavaList;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            int element = scan.nextInt();
+            list.add(element);
+        }
+        int q = scan.nextInt();
+        while (q-- > 0){
+            String query = scan.next();
+            String firstOption = "Insert";
+            String secondOption = "Delete";
+            if (query.equals(firstOption)) {
+                int x = scan.nextInt();
+                int y = scan.nextInt();
+                list.add(x,y);
+            }
+            if (query.equals(secondOption)){
+                int x = scan.nextInt();
+                list.remove(x);
+            }
+        }
+        String listStr = list.toString();
+        String newList= listStr.substring(1, listStr.length()-1).replaceAll(", ", " ");
+        System.out.println(newList);
+        scan.close();
+    }
+}
+
