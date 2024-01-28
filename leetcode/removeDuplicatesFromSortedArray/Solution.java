496d80bd0d7910886cd2c24fbd50ee3ffe496363
betulince <betul.ince@rabobank.nl>
2024-01-28 15:57:23 +0100

    edit folder structure & add leetcode solution

---
+++ leetcode/removeDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.removeDuplicatesFromSortedArray;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int lenghtOfTheList = nums.length;
+
+        // Special case
+        if (lenghtOfTheList <= 2) {
+            return lenghtOfTheList;
+        }
+
+        int currentIndex = 1;
+        int currentElementCount = 1;
+
+        for (int i = 1; i < lenghtOfTheList; i++) {
+
+            if (nums[i] == nums[currentIndex-1]) {
+                currentElementCount++;
+            } else {
+                currentElementCount = 1;
+            }
+
+            if (currentElementCount <= 2) { // Keep adding the value to the current index as long as the occurence of the element is less than or equals to 2.
+                nums[currentIndex] = nums[i];
+                currentIndex++;
+            }
+        }
+        return currentIndex;
+    }
+
+}
